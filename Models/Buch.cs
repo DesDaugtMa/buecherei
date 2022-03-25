36d937b1e3e3659ba285d123831e6e70debfef39
AlexanderFriedrich <friedrich.alexander@gmx.at>
2022-03-25 08:10:23 +0100

    Projektdateien hinzufügen.

---
+++ Models/Buch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buecherei.Models
+{
+    [Table("TblBuch")]
+    public class Buch
+    {
+        [Key]
+        [Required]
+        public string Buchnummer { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string Sachgebiet { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Titel { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Autor { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Ort { get; set; }
+        [Required]
+        public int Erscheinungsjahr { get; set; }
+    }
+}
---
+++ Models/SchuelerIn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buecherei.Models
+{
+    [Table("TblSchuelerIn")]
+    public class SchuelerIn
+    {
+        [Key]
+        [Required]
+        public int Ausweisnummer { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nachname { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Vorname { get; set; }
+    }
+}
